935a217ef65b3922bddb4246a8fc349e7a9bae8b
Carlos Castillo <castillo.st@gmail.com>
2024-01-19 17:42:33 +0100

    ✨ Add ApplicationError struct with a constructor for every error type
    This commit refactors the error handling in the application package. It introduces a new Error type and defines specific error types such as UnexpectedError, InvalidRequest, and LogicError. The ErrInternalServer variable now uses the NewUnexpectedError function to create an instance of the Error type with the message "internal server error". This change will allow a generic way of mapping application exceptions to others with proper error code in the handlers of the infrastructure

--- internal/application/errors.go
+++ internal/application/errors.go
@@ -1,8 +1,44 @@
 package application
 
-import "errors"
-
 var (
 	// ErrInternalServer is returned when an unexpected error occurs
-	ErrInternalServer = errors.New("internal server error")
+	ErrInternalServer = NewUnexpectedError("internal server error")
 )
+
+type ErrorType int
+
+const (
+	UnexpectedError ErrorType = iota
+	InvalidRequest
+	LogicError
+)
+
+type Error struct {
+	errorType ErrorType
+	message   string
+}
+
+func (e *Error) Error() string {
+	return e.message
+}
+
+func NewUnexpectedError(message string) *Error {
+	return &Error{
+		errorType: UnexpectedError,
+		message:   message,
+	}
+}
+
+func NewInvalidRequestError(message string) *Error {
+	return &Error{
+		errorType: InvalidRequest,
+		message:   message,
+	}
+}
+
+func NewLogicError(message string) *Error {
+	return &Error{
+		errorType: LogicError,
+		message:   message,
+	}
+}
